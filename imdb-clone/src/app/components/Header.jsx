4deba3b859fe8caf435844b8776f45d9454b49b1
Wisdom Okogho <wisdommokoghoo@gmail.com>
2024-02-02 17:31:15 +0100

    add dark and light mode functionality

--- imdb-clone/src/app/components/Header.jsx
+++ imdb-clone/src/app/components/Header.jsx
@@ -3,6 +3,7 @@ import MenuItem from './MenuItem'
 import { MdHome } from "react-icons/md";
 import { BsFillInfoCircleFill } from "react-icons/bs";
 import Link from 'next/link';
+import DarkModeSwitch from './DarkModeSwitch';
 
 
 
@@ -13,10 +14,13 @@ export default function Header() {
             <MenuItem title="home" address="/" Icon={<MdHome />}/>
             <MenuItem title="about" address="/about" Icon={<BsFillInfoCircleFill />} />
         </div>
+        <div className='flex items-center gap-4'>
+          <DarkModeSwitch />
         <Link href={"/"} className='flex gap-1 items-center'>
           <span className='text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>IMDb</span>
           <span className='text-xl hidden sm:inline'>Clone</span>
         </Link>
+        </div>
 
     </div>
   )
